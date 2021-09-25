b3e359b14d010f0aef4faf6222011aee9b8442f4
Cuong Nguyen <cuongnguyen291201@gmail.com>
2021-09-25 09:07:34 +0700

    auth v1

---
+++ auth/users/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import redirect, render
+from . forms import UserRegister
+from django.contrib import messages
+from django.contrib.auth import views as auth_view, login
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'users/home.html')
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    else:
+        if request.method == "POST":
+            form = UserRegister(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+
+                messages.add_message(request, messages.SUCCESS, "{username}")
+                return redirect('home', {'username': username})
+        
+        else: 
+            form = UserRegister()
+
+        return render(request, 'users/register.html', {'form': form})
+
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request, 'users/profile.html')
+    else:
+        return redirect('home')
